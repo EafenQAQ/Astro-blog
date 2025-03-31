b739357192d25620691029187fe026ed5a68ca24
EafenQAQ <eafenqaq@gmail.com>
2025-03-31 20:57:20 +0800

    主题切换平滑化

--- src/utilities/ToggleTheme.js
+++ src/utilities/ToggleTheme.js
@@ -5,12 +5,12 @@ const toggleTheme = (e) => {
   let body = document.querySelector("body");
   body.setAttribute("data-theme", newTheme);
 
-  // 根据主题切换背景图片
-  if (newTheme === "synthwave") {
-    body.style.backgroundImage = "url('/assets/backdrop_dark.svg')";
-  } else {
-    body.style.backgroundImage = "url('/assets/backdrop.svg')";
-  }
+  // // 根据主题切换背景图片
+  // if (newTheme === "synthwave") {
+  //   body.style.backgroundImage = "url('/assets/backdrop_dark.svg')";
+  // } else {
+  //   body.style.backgroundImage = "url('/assets/backdrop.svg')";
+  // }
 };
 
 export default toggleTheme;
