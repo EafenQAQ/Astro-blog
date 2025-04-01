d7df10200720d6c041127a096ce6b3326cb69a8c
EafenQAQ <eafenqaq@gmail.com>
2025-04-01 14:30:09 +0800

    向上滚动按钮

---
+++ src/components/ScrolltoTop.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button @click="scrollToTop"
+            class="fixed hover:cursor-pointer z-50    bottom-4 right-4 bg-gray-100 hover:bg-gray-200 p-2 rounded-full">⬆️</button>
+    </div>
+</template>
+
+<script setup>
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+</script>
+
+<style lang="scss" scoped></style>
